d93598d5ce618a58e0359d54833af53482db2719
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-04 08:45:34 -0300

    Terceiro Commit - Código Finalizado

--- numeroaleatorio/src/main/java/numeroaleatorio/Main.java
+++ numeroaleatorio/src/main/java/numeroaleatorio/Main.java
@@ -1,7 +1,28 @@
 package numeroaleatorio;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        
+        System.out.println("Escolha o intervalo em que será gerado o número");
+
+
+        Scanner pergunta = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int num1 = pergunta.nextInt();
+
+
+        System.out.println("Digite o segundo número: ");
+        int num2 = pergunta.nextInt();
+
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(num1, num2);
+
+        System.out.println("Número gerado: " + numeroAleatorio);
+
+        pergunta.close();
+        
+        }
 }
